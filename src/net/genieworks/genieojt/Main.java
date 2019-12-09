14fc57a8f55c47aefabc780dba70c8502ac30230
곽다율 <kdy@genieworks.net>
2019-12-09 10:18:05 +0900

    1-100 랜덤 숫자 맞추기

--- src/net/genieworks/genieojt/Main.java
+++ src/net/genieworks/genieojt/Main.java
@@ -3,6 +3,29 @@ package net.genieworks.genieojt;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int answer = (int) (Math.random()*100) + 1;
+        int input = 0;
+        int count = 0;
+
+        java.util.Scanner s = new java.util.Scanner(System.in);
+
+
+        do {
+            count++;
+            System.out.print("1과 100사이의 값을 입력하세요 : ");
+            input = s.nextInt();
+
+            if(input > answer) {
+                System.out.println("더 작은 수를 입력하세요.");
+                continue;
+            } else if(input < answer) {
+                System.out.println("더 큰 수를 입력하세요.");
+                continue;
+            } else if(input == answer) {
+                System.out.println("맞췄습니다. 시도횟수는"+count+"번 입니다.");
+                break;
+            }
+
+        } while (true);
     }
 }
